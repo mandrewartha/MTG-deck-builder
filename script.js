b33312220da67c6f06937602c337532579b0501b
Kevin Choi <kevchoi1028@gmail.com>
2021-07-06 13:29:24 -0700

    first push

---
+++ script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+//     console.log("hi");
+// }) 
+ 
+// $(function() {
+//     $(".dropdown-trigger").dropdown();
+// })
+
+// $(document).ready(function (dropDown) { 
+//     $(".dropdown-trigger").dropdown();
+// })
